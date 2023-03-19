50d0f91c631ea0777bfa9fc4c38dbdab70dcbbb3
Cris Chong <81703600+tmseldon@users.noreply.github.com>
2023-03-19 00:48:47 -0300

    Adding clause for rotation when it gets similar, same as in the mover case

--- Source/KarundaRaider/RotateObjects.cpp
+++ Source/KarundaRaider/RotateObjects.cpp
@@ -37,17 +37,20 @@ void URotateObjects::TickComponent(float DeltaTime, ELevelTick TickType, FActorC
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 	
-	FRotator CurrentRotation = mOwner->GetActorRotation();
+	const FRotator CurrentRotation = mOwner->GetActorRotation();
 	FRotator TargetRotation = mInitRotation;
 
 	if (bShouldRotate)
 	{
 		TargetRotation = mInitRotation + OffsetRotation;
-
 	}
 
+	if (!CurrentRotation.Equals(TargetRotation, 0.005))
+	{
+		FRotator NewRotation = FMath::RInterpTo(CurrentRotation, TargetRotation, DeltaTime, RotateTime);
+		mOwner->SetActorRotation(NewRotation);
+	}
 
-	
 
 
 	//if (mLimitTime > RotateTime) { return; }
